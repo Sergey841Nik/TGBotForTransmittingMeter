06fc71f116c87f81a17bba91e80939c0e9107e2d
Сергей <nikitos841@yandex.ru>
2025-04-21 21:36:13 +0400

    add config

---
+++ config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent
+
+class Settings(BaseSettings):
+    # Database config
+    DB_LITE: str = "sqlite+aiosqlite:///meter_readings.db"
+    
+    # App config
+    BOT_TOKEN: str
+    ADMIN_IDS: list[int]
+    MODE: str
+    DELTA_MONTH: int = 1
+
+    @property
+    def db_url(self):
+        return self.DB_LITE  # Используем SQLite по умолчанию
+        
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
